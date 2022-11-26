265a7813489a1026ab6c44f8cbc075f1b985758e
Mohamed Kamal <46304271+mohamed-kamal22@users.noreply.github.com>
2022-11-27 00:31:43 +0200

    create

---
+++ assets/js/main.js
@@ -0,0 +1,84 @@
+$(document).ready(function () {
+    if ($(window).width() > 991) {
+        $('.navbar-light .d-menu').hover(function () {
+            $(this).find('.sm-menu').first().stop(true, true).slideDown(300);
+        }, function () {
+            $(this).find('.sm-menu').first().stop(true, true).delay(120).slideUp(100);
+        });
+    }
+
+    $('.latest-items-active').slick({
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 1900,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false,
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            },
+        ]
+    });
+});
+
+
+
+var toTop = 0;
+function autoPlay(){
+  timer = setTimeout(function(){
+    var row = document.getElementById('row');
+    toTop = toTop - 350;
+    if (toTop < -350){
+      toTop = 0;
+      clearTimeout(timer);
+      row.onmouseover = clearTimeout(timer);
+      }
+  row.style.top = toTop +'px';
+  autoPlay();
+  }, 3000);
+}
+autoPlay();
